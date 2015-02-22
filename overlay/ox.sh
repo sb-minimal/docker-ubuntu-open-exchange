dcbbb71c719c71ca0a24c6bd1623b3ce5b8d3777
sb-minimal <sb-minimal@c0n.de>
2015-02-22 22:15:35 +0100

    add some useful open-exchange plugins and allow persistance of /data

--- overlay/ox.sh
+++ overlay/ox.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+randpw(){ < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo;}
 
-DB_PASSWORD=${DB_PASSWORD:-db_password}
-ADMIN_MASTER_PASSWORD=${ADMIN_MASTER_PASSWORD:-admin_master_password}
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-admin}
-ADMIN_EMAIL=${ADMIN_EMAIL:-admin@example.com}
+
+DB_PASSWORD=`randpw`
+ADMIN_MASTER_PASSWORD=`randpw`
 
 	#Memory JVM in MB
 MAX_MEMORY_FOR_JAVAVM=${MAX_MEMORY_FOR_JAVAVM:-1024}
@@ -12,10 +12,40 @@ MAX_MEMORY_FOR_JAVAVM=${MAX_MEMORY_FOR_JAVAVM:-1024}
 	#Filestore size in MB
 FILESTORE_SIZE=${FILESTORE_SIZE:-1000000}
 
+LANG_DEFAULT=${LANG_DEFAULT:-en_US}
+
+MYSQL_NEED_SETUP=no
+
+if [ ! -e /data/mysql ]; then
+	MYSQL_NEED_SETUP=yes
+	echo "Initializing /data/mysql"
+	mv /var/lib/mysql /data
+else
+	rm -r /var/lib/mysql
+fi
+
+ln -s /data/mysql /var/lib/mysql
 /etc/init.d/mysql start
 sleep 10
 
+ETC_NEED_SETUP=no
+if [ ! -e /data/etc ]; then
+	ETC_NEED_SETUP=yes
+	echo "Initializing /data/etc"
+	mv /opt/open-xchange/etc /data
+	echo "export ADMIN_MASTER_PASSWORD=\"$ADMIN_MASTER_PASSWORD\"" > /data/etc/script_settings
+	echo "export DB_PASSWORD=\"$DB_PASSWORD\"" >> /data/etc/script_settings
+else
+	rm -r /opt/open-xchange/etc
+fi
+
+source /data/etc/script_settings
+
+ln -s /data/etc /opt/open-xchange/etc
+	
+
 if [ ! -e /data/initialized ]; then
+	echo "Initializing OX"
 	echo "/opt/open-xchange/sbin/initconfigdb --configdb-pass=$DB_PASSWORD -a"
 	/opt/open-xchange/sbin/initconfigdb --configdb-pass=$DB_PASSWORD -a
 	echo "/opt/open-xchange/sbin/oxinstaller --no-license --servername=oxserver --configdb-pass=$DB_PASSWORD --master-pass=$ADMIN_MASTER_PASSWORD --network-listener-host=localhost --servermemory $MAX_MEMORY_FOR_JAVAVM"
@@ -23,6 +53,10 @@ if [ ! -e /data/initialized ]; then
 	touch /data/initialized
 fi
 
+if [ "$MYSQL_NEED_SETUP" = "yes" ]; then
+		#secure root access by random password
+	mysqladmin -u root password `randpw`
+fi
 
 /etc/init.d/open-xchange start
 sleep 10
@@ -38,6 +72,7 @@ for I in 1 2 3 4 5; do
 done
 
 if [ ! -d /data/filestore ]; then
+	echo "Initializing /data/filestore"
 	mkdir -p /data/filestore
 	chown open-xchange:open-xchange /data/filestore
 	
@@ -46,13 +81,15 @@ if [ ! -d /data/filestore ]; then
 	/opt/open-xchange/sbin/registerdatabase -A oxadminmaster -P $ADMIN_MASTER_PASSWORD -n oxdatabase -p $DB_PASSWORD -m true
 	
 		#create admin
-	/opt/open-xchange/sbin/createcontext -A oxadminmaster -P $ADMIN_MASTER_PASSWORD -c 1 -u oxadmin -d "Context Admin" -g Admin -s User -p $ADMIN_PASSWORD -L defaultcontext -e $ADMIN_EMAIL -q 1024 --access-combination-name=groupware_standard
+	#/opt/open-xchange/sbin/createcontext -A oxadminmaster -P $ADMIN_MASTER_PASSWORD -c 1 -u oxadmin -d "Context Admin" -g Admin -s User -p $ADMIN_PASSWORD -L defaultcontext -e $ADMIN_EMAIL -q 1024 --access-combination-name=groupware_standard
 	
 		#create testuser
-	/opt/open-xchange/sbin/createuser -c 1 -A oxadmin -P $ADMIN_PASSWORD -u testuser -d "Test User" -g Test -s User -p secret -e testuser@example.com --imaplogin testuser --imapserver 127.0.0.1 --smtpserver 127.0.0.1
+	#/opt/open-xchange/sbin/createuser -c 1 -A oxadmin -P $ADMIN_PASSWORD -u testuser -d "Test User" -g Test -s User -p secret -l $LANG_DEFAULT -e testuser@example.com --imaplogin testuser --imapserver 127.0.0.1 --smtpserver 127.0.0.1
 fi
 
 /etc/init.d/apache2 start
+
+trap '/etc/init.d/apache2 stop;/etc/init.d/open-xchange stop;/etc/init.d/mysql stop' TERM INT
 
 while sleep 10; do
 	echo ""
@@ -62,3 +99,4 @@ done
 /etc/init.d/open-xchange stop
 /etc/init.d/mysql stop
 
+sleep 30
